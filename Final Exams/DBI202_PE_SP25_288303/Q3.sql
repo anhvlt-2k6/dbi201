59600b79d63d122c199d711297f45819c53953f6
anhvlt-2k6 <176574466+anhvlt-2k6@users.noreply.github.com>
2025-11-02 22:52:50 +0700

    .

--- Final Exams/DBI202_PE_SP25_288303/Q3.sql
+++
@@ -1,4 +0,0 @@
-SELECT a.AppointmentID, a.AppointmentDate, d.DoctorID, d.FullName AS DoctorFullName, d.Specialty, p.PatientID, (p.FirstName + ' ' + p.LastName) AS PatientFullName
-    FROM MedicalRecords AS m, Appointments AS a, Patients AS p, Doctors AS d
-    WHERE a.PatientID = p.PatientID AND a.AppointmentID = m.AppointmentID AND a.DoctorID = d.DoctorID
-        AND d.Specialty LIKE 'Orthopedics'
--- Final Exams/DBI202_PE_SP25_288303/Q4.sql
+++
@@ -1,5 +0,0 @@
-SELECT d.DoctorID, d.FullName AS DoctorFullName, p.PrescriptionID, p.Medicine, p.Dosage, p.PrescriptionDate, pa.PatientID, (pa.FirstName + ' ' + pa.LastName) AS PatientFullName
-    FROM Doctors AS d
-        LEFT JOIN Prescriptions p ON d.DoctorID = p.DoctorID AND p.PrescriptionDate >= '2020-05-15' AND p.PrescriptionDate <= '2020-09-15'
-        LEFT JOIN Patients pa ON p.PatientID = pa.PatientID
-    ORDER BY d.FullName, p.PrescriptionDate;
